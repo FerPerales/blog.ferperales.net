f3f370f97965cd1dac0a83b510136cefd2c7ab38
Fer Perales <me@ferperales.net>
2020-10-08 21:38:53 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
-JEKYLL_ENV=production jekyll build --incremental
-rsync -v -rz --checksum --delete _site/ $SSH_USERNAME@$HOST:$DESTINATION
+git checkout gh-pages
+JEKYLL_ENV=production jekyll build
+mv _site/ docs/
+git add .
+git commit -m "New release"
+git push origin gh-pages
