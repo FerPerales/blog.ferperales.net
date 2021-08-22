6c966afdec29c45680c3888bd78ddfcbf88e6d9d
Fer Perales <me@ferperales.net>
2021-08-22 12:05:00 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 git checkout gh-pages
+git pull origin gh-pages
+git merge master --no-edit
 JEKYLL_ENV=production jekyll build
-mv _site/ docs/
+rm -rf docs
+mv -f _site/ docs/
+cd docs
+echo 'blog.ferperales.net' > CNAME
 git add .
 git commit -m "New release"
 git push origin gh-pages
+git checkout master
